2fb6aabb710023b82ab16175cccf0f00cbd86209
Peter Kotula <peto.kotula@yahoo.com>
2025-05-09 00:01:44 +0200

    pre commit and rollback - wip

--- golem-worker-executor-base/src/durable_host/rdbms/mod.rs
+++ golem-worker-executor-base/src/durable_host/rdbms/mod.rs
@@ -1124,10 +1124,7 @@ where
         .map(|e| e.state.clone());
 
     match state {
-        Ok(RdbmsTransactionState::Open(transaction)) => {
-            // TODO pre commit
-            Ok(())
-        }
+        Ok(RdbmsTransactionState::Open(transaction)) => transaction.pre_rollback().await,
         Ok(_) => Ok(()),
         Err(error) => Err(RdbmsError::other_response_failure(error)),
     }
@@ -1177,10 +1174,7 @@ where
         .map(|e| e.state.clone());
 
     match state {
-        Ok(RdbmsTransactionState::Open(transaction)) => {
-            // TODO pre rollback
-            Ok(())
-        }
+        Ok(RdbmsTransactionState::Open(transaction)) => transaction.pre_commit().await,
         Ok(_) => Ok(()),
         Err(error) => Err(RdbmsError::other_response_failure(error)),
     }
