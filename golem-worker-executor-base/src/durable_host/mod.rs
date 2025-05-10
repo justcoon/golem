3b5a356f315b7d7ead00841a4b631336e1288a01
Peter Kotula <peto.kotula@yahoo.com>
2025-05-10 23:02:00 +0200

    pre commit and rollback - wip

--- golem-worker-executor-base/src/durable_host/mod.rs
+++ golem-worker-executor-base/src/durable_host/mod.rs
@@ -2763,3 +2763,10 @@ macro_rules! get_oplog_entry {
         }
     };
 }
+
+#[async_trait]
+pub trait RemoteTransactionFinalizer {
+    async fn get_pre_commit_final_entries(&self) -> Result<Vec<OplogEntry>, GolemError>;
+
+    async fn get_pre_rollback_final_entries(&self) -> Result<Vec<OplogEntry>, GolemError>;
+}
