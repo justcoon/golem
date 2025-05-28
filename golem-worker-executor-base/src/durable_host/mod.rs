80956c228ede4d35d8c90eb0a6d8fd3d91575791
Peter Kotula <peto.kotula@yahoo.com>
2025-05-28 22:55:25 +0200

    begin_transaction_function fix, recovery tests - wip

--- golem-worker-executor-base/src/durable_host/mod.rs
+++ golem-worker-executor-base/src/durable_host/mod.rs
@@ -2300,6 +2300,8 @@ impl<Ctx: WorkerCtx> PrivateDurableWorkerState<Ctx> {
                     .try_get_oplog_entry(|e| e.is_pre_remote_transaction(begin_index))
                     .await;
 
+                let mut restart = false;
+
                 if let Some((_, pre_entry)) = pre_entry {
                     let end_index = self
                         .replay_state
@@ -2347,6 +2349,7 @@ impl<Ctx: WorkerCtx> PrivateDurableWorkerState<Ctx> {
                             self.oplog
                                 .add_and_commit(OplogEntry::jump(deleted_region))
                                 .await;
+                            restart = true;
                         }
                     }
                 } else {
@@ -2366,8 +2369,20 @@ impl<Ctx: WorkerCtx> PrivateDurableWorkerState<Ctx> {
                     self.oplog
                         .add_and_commit(OplogEntry::jump(deleted_region))
                         .await;
+                    restart = true;
                 }
-                Ok((begin_index, tx))
+
+                if restart {
+                    let (tx_id, tx) = handler.create_new().await?;
+                    self.oplog
+                        .add_and_commit_safe(OplogEntry::begin_remote_transaction(tx_id))
+                        .await
+                        .map_err(GolemError::runtime)?;
+                    let begin_index = self.oplog.current_oplog_index().await;
+                    Ok((begin_index, tx))
+                } else {
+                    Ok((begin_index, tx))
+                }
             }
         } else {
             Err(GolemError::runtime("Unexpected durable function type").into())
